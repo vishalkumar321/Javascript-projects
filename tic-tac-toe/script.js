14e1ac3d065a1a2d87a6801660922804240e306b
vishal <vishalkumargupta4546@gmail.com>
2025-08-17 11:59:02 +0530

    Add Tic Tac Toe Game - Two-player strategy game with win detection and reset

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,84 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset-btn");
+let container = document.querySelector(".container");
+let newgameBtn = document.querySelector(".newgame-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
+
+let turnO = true;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+]
+
+boxes.forEach((box) =>{
+        box.addEventListener("click",()=>{
+            if(turnO){
+                box.innerText = "O";
+                turnO = false;
+            }else{
+                box.innerText = "X";
+                turnO = true;
+            }
+            box.disabled = true;
+            checkWinner();
+        })
+})
+
+const disableBoxes = () =>{
+    for (const box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () =>{
+    for (const box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const resetGame = () =>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    container.classList.remove("hide");
+    
+}
+
+const showwinner = (winner)=> {
+    msg.innerText = `Congratulation, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    container.classList.add("hide");
+    disableBoxes();
+}
+
+const checkWinner = () =>{
+    for (const pattern of winPatterns) {
+        // console.log(pattern[0],pattern[1],pattern[2]);
+        // console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
+        // console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !="" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val ){
+                console.log("winner",pos2Val);
+                showwinner(pos1Val);
+            }
+        }
+
+    }
+}
+
+resetBtn.addEventListener("click",resetGame);
+newgameBtn.addEventListener("click",resetGame);
